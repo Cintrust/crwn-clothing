396b1a527bc234911fdbc41601ec308785e182d4
Trust <cintrust301@gmail.com>
2021-06-10 23:25:53 +0100

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Switch, Route} from "react-router-dom";
 import HomePage from "./pages/home-page/home-page.component";
 import ShopPage from "./pages/shop-page/shop-page.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+        <Header/>
       <Switch>
         <Route path="/" exact={true} component={HomePage}/>
         <Route path="/shop" exact={true} component={ShopPage}/>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,46 +1,14 @@
 import "./directory.styles.scss"
 import {Component} from "react";
 import MenuItem from "../menu-item/menu-item.component";
+import SECTIONS_DATA from "./sections.data";
 
 export default class Directory extends Component {
     constructor() {
         super();
 
         this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: '/images/hats.png',
-                    id: 1,
-                    linkUrl: 'shop-page/hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: '/images/jackets.png',
-                    id: 2,
-                    linkUrl: 'shop-page/jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: '/images/sneakers.png',
-                    id: 3,
-                    linkUrl: 'shop-page/sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: '/images/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'shop-page/womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: '/images/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop-page/mens'
-                }
-            ]
+            sections: SECTIONS_DATA
         }
     }
 
