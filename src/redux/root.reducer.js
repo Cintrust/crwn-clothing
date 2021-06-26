16a3059f09a0d5ebfb0bcd25c1411355ade1d560
Trust <cintrust301@gmail.com>
2021-06-26 22:33:45 +0100

    added redux persistor

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,11 +1,20 @@
 import {combineReducers} from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default combineReducers({
-    user:userReducer,
-    cart:cartReducer
+const rootReducer = combineReducers({
+    user: userReducer,
+    cart: cartReducer
 })
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["cart"]
+}
+export default persistReducer(persistConfig, rootReducer)
 
 
 
